89b70659500fe94b70468e9bc18b543caded3a1d
zien <sheenazien08@gmail.com>
2019-03-23 14:51:04 +0700

    master CRUD

---
+++ flightplan.js
@@ -0,0 +1,14 @@
+// flightplan.js
+var plan = require('flightplan');
+
+// configuration
+plan.target('staging', {
+  host: '172.104.169.95',
+  username: 'zien-vps',
+  agent: process.env.SSH_AUTH_SOCK
+});
+
+plan.remote(function(transport){
+  transport.exec('cd ~/kasir; git stash; git pull; yarn; yarn run dev; composer install;');
+})
+
